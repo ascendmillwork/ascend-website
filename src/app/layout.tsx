9adafd541a0030dabdfe56245540951b62ab73d8
Vinay Developer <developervsandhu@gmail.com>
2025-07-16 11:36:49 -0400

    Favicon Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,36 @@ export default function RootLayout({
         {/* Canonical Link */}
         <link rel="canonical" href="https://ascendmillwork.com/" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
         {/* Organization & LocalBusiness Structured Data */}
         <script
           type="application/ld+json"
